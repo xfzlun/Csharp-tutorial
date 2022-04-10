61e5a0fe49586f450e30a28d37550beb8815d6ea
Alun Cheng <xfz79@foxmail.com>
2022-04-10 22:28:37 +0800

    Update maincode

---
+++ MyfirstCshartApp1/MyfirstCshartApp1/CSharp_DemoModule/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyfirstCshartApp1.CSharp_DemoModule
+{
+    internal class Class1
+    {
+    }
+}
